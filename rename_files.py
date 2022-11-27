d8058e42bfc73ab077e523371576b6f3759a7856
Praskovsky <91827644+Praskovsky@users.noreply.github.com>
2022-11-27 15:26:15 +0200

    Проблема с переименовкой файлов
    Когда добавляем еще файлы после переименовки - не работает.

--- rename_files.py
+++ rename_files.py
@@ -1,81 +1,78 @@
 from tkinter import *
 from tkinter import filedialog
-from os import *
-'''
-# functions for rename
-def file_extension(file):
-    return file.split('.')[-1]
-'''
-def rename_folder():
+import os
+
+
+def rename_all():
     
-    global name
-    name = file_new.get()
+    new_name = filename_new.get()
     i = 1
-    #folder_files = sorted(os.listdir(folder), key=file_extension)
-    print(name, file_res)
-'''
-    for file in folder_files:
+    #print(new_name, rb.get(), folder, files_array)
+    #print(new_name, rb.get(),folder, folder_files)
+
+    for file in files_array:
         ext = file.split('.')[-1]
-        new_name = f'{name}_{i}.{ext}'
-        if new_name in folder_files:
+        r_name = f'{new_name}_{i}.{ext}'
+        if r_name in files_array:
             i = i + 1
         else:
-            os.rename(f'{folder}/{file}', f'{folder}/{name}_{i}.{ext}')
+            os.rename(f'{folder}/{file}', f'{folder}/{r_name}')
             i = i + 1
-'''
 
-# function for folder
+
+# function for choosing folder
 def choose_folder():
-    pass
-'''
+    
+    global folder
+    global files_array
     dir = filedialog.askdirectory(initialdir='/')
     folder_name.insert(0, dir)
-    global folder
     folder = folder_name.get()
-'''
-# function for files
+    files_array = os.listdir(folder)
+
+
+# function for choosing files
 def choose_files():
     
-    global filenames
+    global folder
+    global files_array
     filenames = filedialog.askopenfilenames(initialdir='/', title="Select files", filetypes=(('JPG files', '*.jpg'), ('all files', '*.*'), ('PDF files', '*.pdf')))
+    folder = '/'.join(filenames[0].split('/')[:-1])
     # красота в окошке файлов.
-    file_res = ', '.join([i.split('/')[-1] for i in filenames])
-    files_lst.insert(0, file_res)
+    files_res = ', '.join([i.split('/')[-1] for i in filenames])
+    files_lst.insert(0, files_res)
+    files_array = files_res.split(', ')
 
-    
-def rename_files():
-    pass
+
     
 window = Tk()
 window.geometry('400x250')
+rb = StringVar()
+rb.set('folder')
 
 # Choose folder
-folder_lbl = Label(window, text="Folder:")
-folder_lbl.grid(column=0, row=0)
+Radiobutton(window, text="Folder", variable=rb, value='folder').grid(column=0, row=0, sticky=W)
 folder_name = Entry(window, width=30, borderwidth=3)
 folder_name.grid(column=1, row=0)
-btn_dir = Button(window, text="Choose folder", command=choose_folder)
-btn_dir.grid(column=2, row=0)
+btn_dir = Button(window, text="Choose folder", padx=15, command=choose_folder)
+btn_dir.grid(column=2, row=0, sticky=W)
 
 # Choose files
-file_lbl = Label(window, text="Files:")
-file_lbl.grid(column=0, row=1)
+Radiobutton(window, text="Files", variable=rb, value='files').grid(column=0, row=1, sticky=W)
 files_lst = Entry(window, width=30, borderwidth=3)
 files_lst.grid(column=1, row=1)
-btn_files = Button(window, text="Choose files", command=choose_files)
-btn_files.grid(column=2, row=1)
+btn_files = Button(window, text="Choose files", padx=20, command=choose_files)
+btn_files.grid(column=2, row=1, sticky=W)
 
 # Enter new name
-name_lbl = Label(window, text="New name:")
-name_lbl.grid(column=0, row=2)
-file_new = Entry(window,width=30, borderwidth=3)
-file_new.grid(column=1, row=2)
-file_new.insert(0, "fotochka")
+newname_lbl = Label(window, text="New name:")
+newname_lbl.grid(column=0, row=2)
+filename_new = Entry(window,width=30, borderwidth=3)
+filename_new.grid(column=1, row=2)
+filename_new.insert(0, "fotochka")
 
-
-
-
-btn = Button(window, text="Переименовать", command=rename_files)
+# Rename button
+btn = Button(window, text="Переименовать", command=rename_all)
 btn.grid(column=1, row=3)
 
 window.mainloop()
